15198aa5cd5db0e46d5369583201c689c57058bd
MirzaSadique101 <mirza.sadique@gmail.com>
2022-06-30 16:21:55 +0530

    Travis's User list final

--- list/travis.py
+++ list/travis.py
@@ -14,7 +14,15 @@ while True:
             print(known_users)
             known_users.remove(name)
             print (known_users)
+        elif remove == "n":
+            print("No problem, I didn't want you to leave anyway!")
     
     
-else:
-    print("Name not recognized")
+    else:
+        print("Hmmm, I don't think I have met you yet {}".format(name))
+        add = input("Do you want your name to be added to the list (y/n): ").strip().lower()
+        if add == "y":
+            known_users.append(name)
+            print("Congratulations!, Your name is added to the list of known users:{} ".format(known_users))
+        elif add == "n":
+            print("No worries, see you around")
