40e965e469a0e3c0d4dba3b85ff1f56d067845a9
MirzaSadique101 <mirza.sadique@gmail.com>
2022-07-16 17:39:28 +0530

    Added If Else condition to handle various status responses

--- motivator.py
+++ motivator.py
@@ -1,3 +1,4 @@
+from nis import match
 import requests
 
 
@@ -5,8 +6,15 @@ def get_quote_of_the_day(category):
 
     url = "https://quotes.rest/qod?language=en&category={}".format(category)
     res = requests.get(url=url)
+    status = res.status_code
     dict_data = res.json()
-    quote = (dict_data['contents']['quotes'][0]['quote'])
+    if status == 200:
+        quote = (dict_data['contents']['quotes'][0]['quote'])
+            
+    elif status == 400:
+            quote = (dict_data['error']['message'])
+    else:
+        print("Sorry, could not get the status code")
     return quote
 
 quote = get_quote_of_the_day(category="inspire")
